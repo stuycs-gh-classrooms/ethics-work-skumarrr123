bdd737e9b97d2a2d2d9f6ebd779777d41c34db84
Suhana Kumar <skumar50@stuy.edu>
2024-09-22 15:02:37 -0400

    add

---
+++ w05_array2d/tictactoe.py
@@ -0,0 +1,18 @@
+from array import *
+win = False
+
+player1 = True
+player2 = False
+game = [3][3]
+print(game)
+# print("Game Instructions: Please input 0, 1, or 2 for row follwed by 0, 1, or 2 for column. Enter your name:")
+# x = input()
+# print("Hello, " + x + ". Your turn ")
+# while (win == False):
+#     if player1:
+#         print("Hello, " + x + ". Enter your row number.")
+#         row = int(input())
+#         print("Hello, " + x + ". Enter your column number.")
+#         col = int(input())
+#         
+#         if choice1 ==
